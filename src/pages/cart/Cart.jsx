ec146f0c0c9967dbcc6038d2a1e1a7232ae5268a
patrick <utlpatrick@yahoo.no>
2023-03-27 15:36:10 +0200

    Update
    Update

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -54,6 +54,7 @@ function Cart({ setCartAmount }) {
         <div className={style.cartItems}>
           {cart.map((id) => {
             return (
+              // eslint-disable-next-line array-callback-return
               products.map((product) => {
                 if (product.id === id) {
                   return (
